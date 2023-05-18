9d7564206211d086d69ba9d67f85a8633ef66cd7
oussama-alaoui <oussamaalaouu@gmail.com>
2023-05-18 05:14:35 +0100

    instal fpm

--- wordpress/tools/script.sh
+++ wordpress/tools/script.sh
@@ -1,1 +1,9 @@
-echo "hello world";
+#!/bin/bash
+
+# change the listen in fpm pool conf to 9000
+# cat /etc/php/7.0/fpm/pool.d/www.conf;
+# sed -i "s/listen = .*/listen = 9000/" /etc/php/7.0/fpm/pool.d/www.conf;
+
+# cat /etc/php/7.0/fpm/pool.d/www.conf;
+echo "Starting PHP-FPM 7.0"
+exec "$@"
