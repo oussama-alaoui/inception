fd68c30d1fb9180d8d7f083f5d19a3a83b13ffd2
Oussama Alaoui chrifi <oalaoui-@e2r4p11.1337.ma>
2023-05-19 17:17:49 +0100

    still error btwen mariadb and wp

--- mariadb/tools/install.sh
+++ mariadb/tools/install.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 sed -i 's/bind-address.*/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
-DB_NAME="test123"
-DB_PSSWRD="1234"
-DB_USER="oalaoui"
-MYSQL_ROOT_PASSWORD="1234"
-service mysql start
-mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME";
-mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PSSWRD'" ;
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%';" ;
-mysql -e "FLUSH PRIVILEGES;"
-mysql -e "alter user 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD';" ;
-mysql -e "FLUSH PRIVILEGES;"
-kill `cat /var/run/mysqld/mysqld.pid`
 
+DB_NAME="xxxx"
+DB_PSSWRD="xxxx"
+DB_USER="xxxx"
+MYSQL_ROOT_PASSWORD="xxxx"
+
+service mysql start
+
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS $DB_NAME"
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PSSWRD'"
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%'"
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES"
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'"
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES"
+#  service mysql stop
+# kill `cat /var/run/mysqld/mysqld.pid`
+rm /var/run/mysqld/mysqld.sock
+service mysql restart
 exec "$@"
--- wordpress/tools/script.sh
+++ wordpress/tools/script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-DB_NAME="test123"
-DB_PSSWRD="1234"
-DB_USER="oalaoui"
+DB_NAME="xxxx"
+DB_PSSWRD="xxxx"
+DB_USER="xxxx"
 DB_HOST="mariadb"
 
 WP_URL="localhost:5050"
@@ -16,10 +16,9 @@ WP_USER_EMAIL="user@alaoui.com"
 
 mkdir -p /run/php   # Create the directory if it doesn't exist
 touch /run/php/php7.3-fpm.pid
-sed -i "s/listen = .*/listen = 9000/" /etc/php/7.3/fpm/pool.d/www.conf
+sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf"
 echo "Starting PHP-FPM 7.0"
 
-Download WordPress CLI
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -O /usr/local/bin/wp
 chmod +x /usr/local/bin/wp
 echo "Downloading WordPress CLI..."
@@ -31,7 +30,8 @@ sed -i "s/database_name_here/$DB_NAME/g" /usr/share/nginx/html/wp-config-sample.
 sed -i "s/username_here/$DB_USER/g" /usr/share/nginx/html/wp-config-sample.php
 sed -i "s/password_here/$DB_PSSWRD/g" /usr/share/nginx/html/wp-config-sample.php
 sed -i "s/localhost/$DB_HOST/g" /usr/share/nginx/html/wp-config-sample.php
-mv /usr/share/nginx/html/wp-config-sample.php /usr/share/nginx/html/wp-config.php
+cd /usr/share/nginx/html/
+cp wp-config-sample.php wp-config.php
 wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PSSWRD --admin_email=$WP_ADMIN_EMAIL --allow-root
 wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PSSWRD --allow-root
 echo "Downloading and configuring WordPress..."
